04e45cde5212ca4add6dc65f208583cfb6166925
ftpmsgx <2519306859@qq.com>
2021-03-09 11:22:53 +0800

    Code refactoring

--- SequenList/SequenList.c
+++ SequenList/SequenList.c
@@ -16,7 +16,9 @@ SqList *initial_SqList() {
 	if (pointer != NULL) {
 		pointer -> end = -1;
 	}
-	printf("Please Enter a number: ");
+	printf("These numbers are used to initialize the Order Table.\n");
+	printf("\033[33m[Warning] Max lenght is 1024.\033[37m\n");
+	printf("Please Enter numbers(0 is end): ");
 	scanf("%d", &num);
 	while (num) {
 		pointer -> data[i++] = num;
@@ -30,6 +32,7 @@ void print_SqList(SqList *pointer) {
 	for (int i = 0; i <= pointer -> end; i++) {
 		printf("%5d", pointer -> data[i]);
 	}
+	printf("\n");
 }
 
 int insert_SqList(SqList *pointer, int location, int number) {
@@ -64,12 +67,46 @@ void reverse_SqList(SqList *pointer) {
 
 int main() {
 	SqList *pointer = initial_SqList();
-	reverse_SqList(pointer);
-	print_SqList(pointer);
-	printf("\n");
-	// int status = insert_SqList(pointer, 2, 60);
-	// printf("Status Code : %d\n", status);
-	// print_SqList(pointer);
-	// printf("\n");
+	for (;;) {
+		int select;
+		printf("-------------------------------\n");
+		printf("1. Print Sequen List\n");
+		printf("2. Insert a Number\n");
+		printf("3. Reverse Sequen List\n");
+		printf("4. Reinitializes Order Table\n");
+		printf("5. Exit\n");
+		printf("-------------------------------\n");
+		printf("Enter 1 to 5:");
+		scanf("%d", &select);
+		switch (select) {
+			case 1:
+				print_SqList(pointer);
+				break;
+			case 2:
+				printf("Enter location:");
+				int location;
+				scanf("%d", &location);
+				printf("Enter Number:");
+				int num;
+				scanf("%d", &num);
+				int status = insert_SqList(pointer, location, num);
+				printf("Status Code : %d\n", status);
+				print_SqList(pointer);
+				printf("\n");
+				break;
+			case 3:
+				reverse_SqList(pointer);
+				printf("\n");
+				break;
+			case 4:
+				pointer = initial_SqList();
+				break;
+			case 5:
+				printf("The program has exited normally.(Status Code : 0)\n");
+				exit(0);
+			default:
+				printf("\033[31m[Error] at selection number!\033[37m\n");
+		}
+	}
 	return 0;
 }
