2615b6bd0685863051368c8ceadd1e5ca63673d5
ftpmsgx <2519306859@qq.com>
2021-03-09 18:57:08 +0800

    New function

--- SequenList/SequenList.c
+++ SequenList/SequenList.c
@@ -29,6 +29,7 @@ SqList *initial_SqList() {
 }
 
 void print_SqList(SqList *pointer) {
+	printf("Order table: \n");
 	for (int i = 0; i <= pointer -> end; i++) {
 		printf("%5d", pointer -> data[i]);
 	}
@@ -37,11 +38,11 @@ void print_SqList(SqList *pointer) {
 
 int insert_SqList(SqList *pointer, int location, int number) {
 	if (pointer -> end >= MAX - 1) {
-		printf("This SqList is full!\n");
+		printf("\033[31m[Error] This table is full!\n\033[37m");
 		return -1;
 	}
 	if (location >= pointer -> end + 2) {
-		printf("Position out of range.\n");
+		printf("\033[31m[Error] Position out of range.\n\033[37m");
 		return 1;
 	}
 	for (int j = pointer -> end; j >= location; j--) {
@@ -54,7 +55,7 @@ int insert_SqList(SqList *pointer, int location, int number) {
 
 void reverse_SqList(SqList *pointer) {
 	if (pointer -> end == -1) {
-		printf("This table is empty!\n");
+		printf("\033[31m[Error] This table is empty!\n\033[37m");
 		return;
 	}
 	int temp;
@@ -62,6 +63,57 @@ void reverse_SqList(SqList *pointer) {
 		temp = pointer -> data[i];
 		pointer -> data[i] = pointer -> data[j];
 		pointer -> data[j] = temp;
+	}
+	print_SqList(pointer);
+}
+
+int query_SqList(SqList *pointer, int number) {
+	if (pointer -> end == -1) {
+		printf("\033[31m[Error] This table is empty!\n\033[37m");
+		return -1;
+	}
+	int flag = 0;
+	int i, j;
+	for (i = 0; i <= pointer -> end; i++) {
+		if (pointer -> data[i] == number) {
+			flag = 1;
+			break;
+		}
+	}
+	if (flag) {
+		printf("The physics location of this number: %d\n", i);
+		printf("The logic location of this number: %d\n", i + 1);
+		return i;
+	} else {
+		printf("\033[31m[404]Not found this number!\n\033[37m");
+		return -2;
+	}
+}
+
+void delete_SqList(SqList *pointer) {
+	printf("Please Enter a number that you want to delete: ");
+	if (pointer -> end == -1) {
+		printf("\033[31m[Error] This table is empty!\n\033[37m");
+		return;
+	}
+	int number;
+	scanf("%d", &number);
+	int flag = query_SqList(pointer, number);
+	if (flag >= 0) {
+		int i;
+		for (i = flag; i < pointer -> end; i++) {
+			pointer -> data[i] = pointer -> data[i + 1];
+		}
+		pointer -> data[i] = 0;
+		pointer -> end--;
+		printf("\033[32mDelete was successfully!\n\033[37m");
+		print_SqList(pointer);
+	} else if (flag == -1) {
+		printf("\033[31m[Error] Delete failed!\n");
+		printf("\033[31m[Error] This number was not found!\n\033[37m");
+	} else {
+		printf("\033[31m[Error] Delete failed!\n");
+		printf("\033[31m[Error] This table is empty!\n\033[37m");
 	}
 }
 
@@ -74,22 +126,24 @@ int main() {
 		printf("2. Insert a Number\n");
 		printf("3. Reverse Sequen List\n");
 		printf("4. Reinitializes Order Table\n");
-		printf("5. Exit\n");
+		printf("5. Query a Number\n");
+		printf("6. Delete a Number\n");
+		printf("7. Exit\n");
 		printf("-------------------------------\n");
-		printf("Enter 1 to 5:");
+		printf("Enter 1 to 7:");
 		scanf("%d", &select);
 		switch (select) {
 			case 1:
 				print_SqList(pointer);
 				break;
 			case 2:
-				printf("Enter location:");
+				printf("Enter location(Logic position):");
 				int location;
 				scanf("%d", &location);
 				printf("Enter Number:");
 				int num;
 				scanf("%d", &num);
-				int status = insert_SqList(pointer, location, num);
+				int status = insert_SqList(pointer, location - 1, num);
 				printf("Status Code : %d\n", status);
 				print_SqList(pointer);
 				printf("\n");
@@ -102,6 +156,15 @@ int main() {
 				pointer = initial_SqList();
 				break;
 			case 5:
+				printf("Please Enter a number that you want to Query: ");
+				int query_Num;
+				scanf("%d", &query_Num);
+				int clearStatusCode = query_SqList(pointer, query_Num);
+				break;
+			case 6:
+				delete_SqList(pointer);
+				break;
+			case 7:
 				printf("The program has exited normally.(Status Code : 0)\n");
 				exit(0);
 			default:
